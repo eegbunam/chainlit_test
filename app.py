40bb6fcbd6177b9523065f2f15b76eda3a02182f
Ebuka Egbunam <ebukaegb@meta.com>
2024-12-02 20:14:09 -0800

    mile stone 4

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import chainlit as cl
 import openai
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv() 
 
 api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY environment variable is not set")
+
 
 endpoint_url = "https://api.openai.com/v1"
 print(api_key)
@@ -15,20 +22,25 @@ model_kwargs = {
     "max_tokens": 500
 }
 
-@cl.on_message
-async def on_message(message: cl.Message):
-    # Your custom logic goes here...
+# api_key = os.getenv("OPENAI_API_KEY")
 
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": message.content}],
-        **model_kwargs
-    )
-    print(api_key)
-    print(response)
+api_key = os.getenv("RUNPOD_API_KEY")
+runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
 
-    # https://platform.openai.com/docs/guides/chat-completions/response-format
-    response_content = response.choices[0].message.content
+# endpoint_url = "https://api.openai.com/v1"
+endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
 
-    await cl.Message(
-        content=response_content,
-    ).send()
+client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
+
+# https://platform.openai.com/docs/models/gpt-4o
+# model_kwargs = {
+#     "model": "chatgpt-4o-latest",
+#     "temperature": 1.2,
+#     "max_tokens": 500
+# }
+
+model_kwargs = {
+    "model": "mistralai/Mistral-7B-Instruct-v0.3",
+    "temperature": 0.3,
+    "max_tokens": 500
+}
